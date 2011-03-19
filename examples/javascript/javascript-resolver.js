30b5b631ea0bf7979fca46d00ca6d0e9d9c378f5
Christian Muehlhaeuser <muesli@gmail.com>
2011-03-19 09:08:27 +0100

    * Added JavaScript example resolver.

---
+++ examples/javascript/javascript-resolver.js
@@ -0,0 +1,41 @@
+/*
+ * (c) 2011 Christian Muehlhaeuser <muesli@tomahawk-player.org>
+ *
+ */
+
+function getSettings()
+{
+    // Initialize the resolver
+    var response = new Object();
+    response.name = "JavaScript Example Resolver";
+    response.weight = 80;
+    response.targettime = 10;
+    response.timeout = 15;
+
+    return response;
+}
+
+
+function resolve( qid, artist, album, track )
+{
+    var result = new Object();
+    result.artist = "Mokele";
+    result.album = "You Yourself are Me Myself and I am in Love";
+    result.track = "Hiding In Your Insides (php)";
+    result.source = "Mokele.co.uk";
+    result.url = "http://play.mokele.co.uk/music/Hiding%20In%20Your%20Insides.mp3";
+    result.bitrate = 160;
+    result.duration = 248;
+    result.size = 4971780;
+    result.score = 1.0;
+    result.extension = "mp3";
+    result.mimetype = "audio/mpeg";
+    results.push(result);
+
+    // prepare the response
+    var response = new Object();
+    response.qid = qid;
+    response.results = results;
+
+    return response;
+}
