2e561176057e40cdd7a662d0515c50423d3c0951
Leo Franchi <lfranchi@kde.org>
2011-08-29 00:11:04 -0400

    Initial working GS resolver. Still needs work, but tracks play

--- grooveshark/grooveshark.js
+++ grooveshark/grooveshark.js
@@ -1,13 +1,3 @@
-function getSettings()
-{
-    var response = new Object();
-    response.name = "Grooveshark Resolver";
-    response.weight = 85;
-    response.timeout = 5;
-
-    return response;
-}
-
 // : Start a session using startSession and then use authenticate to authenticate your user.
 // Verify that they are an Anywhere user and then proceed. Be sure to keep the sessionID
 //    generated from startSession and send it along with all stream requests.
@@ -18,56 +8,229 @@ function getSettings()
 // 5. Make a request to getStreamKeyStreamServer or getSubscriberStreamKey with your sessionID, songID and country.
 //     You can use lowBitrate=1 to get a low bitrate file, otherwise send lowBitrate=0.
 
-function getSessionId() {
-    var sessionId = window.localStorage['sessionId'];
-    if (sessionId) {
-        return sessionId;
-    }
+var GroovesharkResolver = Tomahawk.extend(TomahawkResolver,
+{
+    secret : "499ca17500cd8e20afb1613c2d264e7e",
+    apiKey : "tomahawkplayer",
+    sessionId : "",
+    streamKeys: [],
+    
+        // 
+        // cleanTitle: function(artist, title){
+        //  if (title.search("\\[|\\]|\\(|\\)|\\*|\\+|\\?|\\/") != 1){
+        //      title = title.replace(new RegExp("\\[|\\]|\\(|\\)|\\*|\\+|\\?|\\/", "gi"), "");
+        //  }
+        //  var stripArtist = new RegExp("\\W*[by]*[the]*\\W*"+artist+"\\W*", "gi");
+        //  var stripAppendingQuotes = new RegExp("\"", "gi");
+        //  if (title.search(new RegExp(artist, "gi")) != -1 && 
+        //      title.search(new RegExp(title, "gi")) != 1){
+        //      if (title.search(stripArtist) != -1){
+        //          title = title.replace(stripArtist, "").trim();
+        //          if (title.search(stripAppendingQuotes) == title.length - 1  && title.search(stripAppendingQuotes) != 0){
+        //              title = title.replace(stripAppendingQuotes, "").trim();
+        //          }
+        //          if (title.search(stripAppendingQuotes) != title.length - 1  && title.search(stripAppendingQuotes) == 0){
+        //              title = title.replace(stripAppendingQuotes, "").trim();
+        //          }
+        //      }
+        //      return title;
+        //  }
+        // },
+        
+	settings:
+	{
+		name: 'Grooveshark',
+		weight: 85,
+		timeout: 5
+	},
+	getConfigUi: function()
+    {
+        var uiData = Tomahawk.readBase64("config.ui");
+        return {
 
-    var sessionUrl = "http://api.grooveshark.com/ws3.php"
-}
+            "widget": uiData,
+            fields: [
+                { name: "username", widget: "usernameLineEdit", property: "text" },
+                { name: "password", widget: "passwordLineEdit", property: "text" },
+            ],
+            images: [
+                { "grooveshark.png" : Tomahawk.readBase64("grooveshark.png") },
+            ]
+        };
+    },
+    newConfigSaved: function() {
+        var userConfig = this.getUserConfig();
+        if((userConfig.username != this.username) ||
+           (userConfig.password != this.password)) {
+               this.username = userConfig.username;
+               this.userpassword = userConfig.password;
+               
+               this.authenticate();
+           }
+    },
+	apiCall: function(methodName, args, callback)
+	{
+	    var payload = {method: methodName};
+	    payload.header = { wsKey: this.apiKey }
+	    if( this.sessionId != "" ) {
+	        payload.header.sessionID = this.sessionId;
+        }
+        payload.parameters = args;
+        
+	    var json = JSON.stringify(payload);
+	    var sig = Tomahawk.hmac(this.secret, json);
+	    var url = "https://api.grooveshark.com/ws/3.0/?sig=" + sig;
+		this.doPost(url, json, callback);
+	},
+    doPost: function(url, body, callback)
+    {
+        var xmlHttpRequest = new XMLHttpRequest();
+        xmlHttpRequest.open('POST', url, true);
+        xmlHttpRequest.setRequestHeader("Content-Type", "application/octet-stream");
+        xmlHttpRequest.onreadystatechange = function() {
+            
+            if (xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200) {
+                callback.call(window, xmlHttpRequest);
+            } else if (xmlHttpRequest.readyState === 4) {
+                Tomahawk.log("Failed to do POST request: to: " + url + " and with body: " + body);
+                Tomahawk.log("Status Code was: " + xmlHttpRequest.status);
+                if(this.sessionId) { // if an error occurred, try re-fetching session id once
+                    this.getSessionId();
+                }
+            }
+        }
+        Tomahawk.log("Post Body: " + body);
+        xmlHttpRequest.send(body);
+    },
 
-function resolve( qid, artist, album, track ){
+    init : function() {
+        var userConfig = this.getUserConfig();
+        if( !userConfig.username || !userConfig.password )
+        {
+            Tomahawk.log("Ampache Resolver not properly configured!");
+            return;
+        }
+        this.username = userConfig.username;
+        this.password = userConfig.password;
+          
+        this.sessionId = window.localStorage['sessionId'];
+        this.countryId = window.localStorage['countryId'];
+        
+        if (!this.sessionId) {
+            this.getSessionId();
+        } else if( !this.countryId) {
+            this.getCountry();
+        }
+        
+        this.getCountry();
+    },
+    
+    getSessionId: function() {
+        var that = this;
+        this.apiCall("startSession", [], function(xhr) {
+            var res = JSON.parse(xhr.responseText);
+            if(res.result.success) {
+                that.sessionId = res.result.sessionID;
+                window.localStorage['sessionId'] = that.sessionId;
+                that.authenticate();
+            } else {
+                Tomahawk.log("Not able to get session id.. " + xhr.responseText);
+            }
+        });
+    },
+    authenticate: function() {
+        var params = {
+            login: this.username,
+            password: Tomahawk.md5(this.password)
+        };
+        this.apiCall("authenticate", params, function(xhr) {
+            Tomahawk.log("Got result of authenticate: " + xhr.responseText);
+            var ret = JSON.parse(xhr.responseText);
+            if(ret.result.success) {
+                if(!ret.result.IsAnywhere) {
+                    alert("Tomahawk requires a Grooveshark Anywhere account!");
+                    return;
+                } else if (!this.countryId) {
+                    this.getCountry();
+                }
+            }
+        });
+    },
+    
+    getCountry: function() {
+        this.apiCall('getCountry', [], function(xhr) {
+            var ret = JSON.parse(xhr.responseText);
+            this.countryId = JSON.stringify(ret.result);
+            Tomahawk.log("Got country id: " + this.countryId);
+            window.localStorage['countryId'] = this.countryId;
+            
+            // Finally ready
+        });
+    },
+    
+    resolve: function(qid, artist, album, title)
+    {
+        if(!this.countryId)
+        {
+            return;
+        }
+        
+        var params = {
+            query: title,
+            country: this.countryId,
+            limit: 10
+        };
+        var that = this;
+        this.apiCall("getSongSearchResults", params, function(xhr)
+        {
+            Tomahawk.log("Got song search results: " + xhr.responseText);
+            var ret = JSON.parse(xhr.responseText);
+            if(!ret || !ret.result || !ret.result.songs)
+                return;
+            var songs = JSON.parse(xhr.responseText).result.songs;
+            // TODO for now just taking the first one and getting the streaming url/key.
+            if(songs.length > 0) {
+                var song = songs[0];
+                var params = {
+                    songID: song.SongID,
+                    country: JSON.parse(that.countryId),
+                    lowBitrate: 0
+                    };
+                    
+                that.apiCall('getSubscriberStreamKey', params, function(xhr) {
+                    Tomahawk.log("Got song stream server: " + xhr.responseText);
+                    var ret = JSON.parse(xhr.responseText);
+                    if(ret.errors) {
+                        Tomahawk.log("Got error doing getSubscriberStreamKey api call: " + xhr.responseText);
+                    } else {
+                        var result = {
+                            artist: song.ArtistName,
+                            album: song.AlbumName,
+                            track: song.SongName,
+                            source: that.settings.name,
+                            url: unescape(ret.result.url),
+                            mimetype: 'audio/mpeg',
+                            duration: ret.result.uSecs/1000000,
+                            // score: Tomahawk.valueForSubNode(song, "rating")
+                        };
+                        Tomahawk.log("Found MP3 URL: " + result.url);
+                        that.streamKeys[result.url] = ret.result.StreamKey;
+                        
+                        var toReturn = {
+                            results: [result],
+                            qid: qid
+                        };
+                        Tomahawk.addTrackResults(toReturn);
+                    }
+                } );
+            }
+        });
+    },
+	search: function( qid, searchString )
+	{
+		// Soundcloud can't return an artist thus search is disabled for this resolver, sorry
+        // return this.parseSongResponse(qid, "", "");
+	}
+});
 
-  var url = "http://api.soundcloud.com/tracks.json?consumer_key=TiNg2DRYhBnp01DA3zNag&filter=streamable&q=";
-
-  if(artist !== "" ){
-    url += encodeURIComponent(artist) + "+";
-  }
-  if(track !== "" ){
-    url += encodeURIComponent(track);
-  }
-  url = url.replace(/\%20/g,'\+');
-  var myJsonObject = {};
-  var httpRequest = new XMLHttpRequest();
-  httpRequest.open("GET", url, false);
-  httpRequest.onreadystatechange = function(){
-    if (httpRequest.readyState == 4 && httpRequest.status == 200){
-	myJsonObject = JSON.parse(httpRequest.responseText);
-    }
-  }
-  httpRequest.send(null);
-
-  var results = new Array();
-
-  for (i=0;i<myJsonObject.length;i++){
-    var result = new Object();
-    result.artist = artist;
-    result.track = myJsonObject[i].title;
-    result.year = myJsonObject[i].release_year;
-    result.source = "SoundCloud";
-    result.url = myJsonObject[i].stream_url+".json?client_id=TiNg2DRYhBnp01DA3zNag";
-    result.mimetype = "audio/mpeg";
-    result.bitrate = 128;
-    result.duration = myJsonObject[i].duration/1000;
-    result.score = 1.00;
-    results.push(result);
-  }
-
-  var response = new Object();
-  response.qid = qid;
-  response.results = results;
-
-  return response;
-
-}
+Tomahawk.resolver.instance = GroovesharkResolver; 
