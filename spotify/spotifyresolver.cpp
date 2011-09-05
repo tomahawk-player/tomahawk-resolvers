e4ce89fcae665fdbb5784b1ccf2bcd03eac71d56
Leo Franchi <lfranchi@kde.org>
2011-09-05 11:24:13 -0400

    Stay compatible with libspotify 0.0.8 as well

--- spotify/spotifyresolver.cpp
+++ spotify/spotifyresolver.cpp
@@ -518,7 +518,11 @@ void SpotifyResolver::login()
 {
     if( !m_username.isEmpty() && !m_pw.isEmpty() ) { // log in
         qDebug() << "Logging in with username:" << m_username;
+#if SPOTIFY_API_VERSION > 8
         sp_session_login(m_session, m_username.toLatin1(),  m_pw.toLatin1(), false);
+#else
+        sp_session_login(m_session, m_username.toLatin1(),  m_pw.toLatin1());
+#endif
     }
 }
 
