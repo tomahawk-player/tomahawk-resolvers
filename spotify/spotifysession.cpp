6d7e902849b68b6629f05fc4d91ebeaeef2d83b0
Hugo Lindström <hugolm84@gmail.com>
2012-03-14 16:02:45 +0100

    Dont set QApplication settings twice.

--- spotify/spotifysession.cpp
+++ spotify/spotifysession.cpp
@@ -39,9 +39,9 @@ SpotifySession::SpotifySession( sessionConfig config, QObject *parent )
     // Set applicationName
     // @note: this is needed for setting syncflags and keep them on shutdown.
     // sure it can be combined with the resolver better
-    QCoreApplication::setOrganizationName("org.SpotifyApi");
+    /*QCoreApplication::setOrganizationName("org.SpotifyApi");
     QCoreApplication::setOrganizationDomain("spotifyapi-git");
-    QCoreApplication::setApplicationName("SpotifyApi");
+    QCoreApplication::setApplicationName("SpotifyApi");*/
 
     // Instance
     s_instance = this;
