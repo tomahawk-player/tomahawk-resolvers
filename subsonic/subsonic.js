dd6a3be5dc6d297aae7b797d9da5632f381f04bb
Dominik Schmidt <dev@dominik-schmidt.de>
2012-03-18 17:08:14 +0100

    subsonic: remove trailing whitespaces

--- subsonic/subsonic.js
+++ subsonic/subsonic.js
@@ -1,6 +1,6 @@
 
 var SubsonicResolver = Tomahawk.extend(TomahawkResolver, {
-    
+
     getConfigUi: function () {
         var uiData = Tomahawk.readBase64("config.ui");
         return {
@@ -27,27 +27,27 @@ var SubsonicResolver = Tomahawk.extend(TomahawkResolver, {
             }]
         };
     },
-    
+
     newConfigSaved: function () {
         var userConfig = this.getUserConfig();
         Tomahawk.log("newConfigSaved User: " + userConfig.user);
-        
+
         if (this.user !== userConfig.user ||
             this.password !== userConfig.password ||
             this.subsonic_url !== userConfig.subsonice_url ||
             this.subsonic_api !== userConfig.subsonic_api)
-        {    
+        {
             this.init();
         }
     },
-    
+
     settings:
     {
         name: 'Subsonic',
         weight: 70,
         timeout: 15
     },
-   
+
     encodePassword : function(password)
     {
         var hex_slice;
@@ -60,14 +60,14 @@ var SubsonicResolver = Tomahawk.extend(TomahawkResolver, {
         }
         return "enc:" + hex_string;
     },
-    
+
     init: function() {
         var userConfig = this.getUserConfig();
         if (!userConfig.user || !userConfig.password) {
             Tomahawk.log("Subsonic Resolver not properly configured!");
             return;
         }
-        
+
         Tomahawk.log("Doing Subsonic resolver init, got credentials from config.  User: " + userConfig.user);
         this.user = userConfig.user;
         var enc_password = this.encodePassword(userConfig.password);
@@ -75,7 +75,7 @@ var SubsonicResolver = Tomahawk.extend(TomahawkResolver, {
         this.subsonic_url = userConfig.subsonic_url.replace(/\/+$/, "");
         this.subsonic_api = userConfig.subsonic_api;
     },
-    
+
     getXmlAttribute: function(attrib_name, attributes) {
         for (var count = 0; count < attributes.length; ++count)
         {
@@ -84,7 +84,7 @@ var SubsonicResolver = Tomahawk.extend(TomahawkResolver, {
         }
         return null;
     },
-    
+
     buildBaseUrl : function(subsonic_view)
     {
         var subsonic_api = this.subsonic_api;
@@ -99,7 +99,7 @@ var SubsonicResolver = Tomahawk.extend(TomahawkResolver, {
         var base_url = this.subsonic_url + subsonic_view + "?u=" + this.user + "&p=" + this.password + "&v=" + subsonic_api + "&c=tomahawk";
         return base_url;
     },
-    
+
     parseSongFromAttributes : function(song_attributes)
     {
         return {
@@ -116,34 +116,34 @@ var SubsonicResolver = Tomahawk.extend(TomahawkResolver, {
             year: this.getXmlAttribute("year", song_attributes)
         };
     },
-    
+
     executeSearchQuery : function(qid, search_url, song_xml_tag, limit)
     {
         var results = [];
         var that = this; // needed so we can reference this from within the lambda
-        
+
         // Important to recognize this async request is doing a get and the user / password is passed in the search url
         // TODO: should most likely just use the xhr object and doing basic authentication.
         Tomahawk.asyncRequest(search_url, function(xhr) {
             var dom_parser = new DOMParser();
             xmlDoc = dom_parser.parseFromString(xhr.responseText, "text/xml");
             Tomahawk.log(xhr.responseText);
-            
+
             var search_results = xmlDoc.getElementsByTagName(song_xml_tag);
             for (var count = 0; count < Math.min(search_results.length, limit); count++)
             {
                 results.push(that.parseSongFromAttributes(search_results[count].attributes));
             }
-            
+
             var return_songs = {
                 qid: qid,
                 results: results
             };
-            
+
             Tomahawk.addTrackResults(return_songs);
         });
     },
-   
+
     //! Please note i am using the deprecated search method in resolve
     //  The reason i am doing this is because it allows me to get a little more specific with the search
     //  since i have the artist, album and title i want to be as specific as possible
@@ -154,7 +154,7 @@ var SubsonicResolver = Tomahawk.extend(TomahawkResolver, {
         var search_url = this.buildBaseUrl("/rest/search.view") + "&artist=" + artist + "&album=" + album + "&title=" + title + "&count=1";
         this.executeSearchQuery(qid, search_url, "match", 1);
     },
-    
+
     search: function( qid, searchString )
     {
         var search_url = this.buildBaseUrl("/rest/search2.view") + "&query=" + encodeURIComponent(searchString);
