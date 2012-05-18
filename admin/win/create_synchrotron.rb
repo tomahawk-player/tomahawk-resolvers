e59c21284a1cc7fe82319d1ea3213a88e594f9ff
Leo Franchi <leo@kdab.com>
2012-05-18 14:36:12 -0400

    Finish windows binary synchrotron script

--- admin/win/create_synchrotron.rb
+++ admin/win/create_synchrotron.rb
@@ -10,22 +10,23 @@
 #
 require 'pathname'
 
-zipper = "C:\Program Files\7-Zip\7z.exe"
+zipper = "C:\\Program Files\\7-Zip\\7z.exe"
 
-if ARGV.length < 3
-  puts "Usage: ruby create_synchrotron_win.rb /path/to/resolver_folder resolver_name.exe metadata.desktop private_key_file"
+if ARGV.length < 5
+  puts "Usage: ruby create_synchrotron_win.rb /path/to/resolver_folder resolver_name.exe metadata.desktop private_key_file /path/to/tomahawk-synchrotron"
   puts "\nCall this from the build directory."
   puts "If you don't have the tomahawk private key and you think you should, ask leo :)"
   exit
 end
 
-if not File.directory?(ARGV[0]) or not File.exists?(ARGV[1]) or not File.exists?(ARGV[2]) or not File.exists?(ARGV[3])
+if not File.directory?(ARGV[0]) or not File.exists?(ARGV[2]) or not File.exists?(ARGV[3]) or not File.directory?(ARGV[4])
   puts "One of your arguments didn't exist!"
   exit
 end
 
 fullPath = File.join(ARGV[0], ARGV[1])
 resolver = ARGV[1].split("_")[0]
+platform = "win"
   
 puts "Creating zipfile for #{resolver} in folder #{fullPath}..."
 
@@ -34,20 +35,28 @@ folder = "spotify-win"
 `copy #{ARGV[0]} #{folder}`
 
 tarball = "#{resolver}.zip"
-`#{zipper} a -tzip #{tarball} #{folder}`
+`"#{zipper}" a -tzip #{tarball} #{folder}`
 `del /q #{folder}`
 `rd #{folder}`
 
-puts "Signing..."
-signature = `openssl dgst -sha1 -binary < "#{tarball}" | openssl dgst -dss1 -sign "#{ARGV[2]}" | openssl enc -base64`
+#puts "Signing... openssl dgst -sha1 -binary < \"#{tarball}\" | openssl dgst -dss1 -sign \"#{ARGV[3]}\" | openssl enc -base64"
+signature = `openssl dgst -sha1 -binary < "#{tarball}" | openssl dgst -dss1 -sign "#{ARGV[3]}" | openssl enc -base64`
 puts "Signature: #{signature}"
 
-fd = File.open(ARGV[1], 'r')
+fd = File.open(ARGV[2], 'r')
 manifest = fd.read
 
 manifest["_SIGNATURE_"] = signature
 manifest["_ZIPFILE_"] = tarball
+manifest["_PLATFORM_"] = platform
+manifest["_TYPE_"] = platform
+manifest["_BINARY_"] = ARGV[1]
 
-File.open("#{resolver}.desktop", "w") do |f|
+resolverDir = "#{ARGV[4]}\\resolvers\\#{resolver}-#{platform}\\content"
+`mkdir #{resolverDir}` if not File.directory?(resolverDir)
+
+File.open("#{resolverDir}/metadata.desktop", "w") do |f|
   f.write(manifest)
 end
+
+`move #{tarball} #{resolverDir}`
