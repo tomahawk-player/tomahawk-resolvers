8781bfc1b940230b7218f4c033c77ec40dfd5e85
Leo Franchi <lfranchi@kde.org>
2012-05-21 18:04:36 -0400

    Add static build script

---
+++ spotify/build_static.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Building static binary. Please make sure you have a libdl.a in this directory as well as a libspotify.so.11."
+echo "Remove any libspotify.so installed in system-wide paths, as well."
+mkdir static
+pushd static
+cp ../libdl.a .
+cp ../libspotify.so.11 .
+ln -s libspotify.so.11 libspotify.so
+cmake -DQJSON_INCLUDE_DIR=/home/leo/kde-static/include/ -DQJSON_LIBRARIES=/home/leo/kde-static/lib/libqjson.a ..
+make -j10
+popd
