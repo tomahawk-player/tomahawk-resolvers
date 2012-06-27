4c6f38533db9327d5573dbc567e4bd4104e66769
Leo Franchi <lfranchi@kde.org>
2012-06-27 12:29:39 -0400

    use libspotify v12

--- spotify/build_static.sh
+++ spotify/build_static.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-echo "Building static binary. Please make sure you have a libdl.a in this directory as well as a libspotify.so.11."
+echo "Building static binary. Please make sure you have a libdl.a in this directory as well as a libspotify.so.12."
 echo "Remove any libspotify.so installed in system-wide paths, as well."
 echo "\n\n"
 
 mkdir static
 pushd static
 cp ../libdl.a .
-cp ../libspotify.so.11 .
-ln -s libspotify.so.11 libspotify.so
+cp ../libspotify.so.12 .
+ln -s libspotify.so.12 libspotify.so
 cmake -DQJSON_INCLUDE_DIR=/home/leo/kde-static/include/ -DQJSON_LIBRARIES=/home/leo/kde-static/lib/libqjson.a ..
 make -j10
 popd
