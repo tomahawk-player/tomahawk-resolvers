e9b97da0e6458975a1c0c8508fde8e1616f99610
Leo Franchi <lfranchi@kde.org>
2012-06-30 17:46:49 -0400

    Build statically on os x

--- admin/mac/create_synchrotron.rb
+++ admin/mac/create_synchrotron.rb
@@ -3,15 +3,14 @@
 #   This takes a binary for osx and creates
 #    the signed zip file + manifest suitable for uploading to Synchrotron
 #
-#  Note that this assumes the Tomahawk.app bundle contains libspotify and qjson.
+# Note that this assumes the resolver has been built against a STATIC qt and qjson.
+# See static_notes.txt in spotify/ for the qt configure line to use.
 #
 require 'pathname'
 
-QT_VERSION = "4.8.0"
 LIBSPOTIFY_VERSION = "#{`brew ls -version libspotify | tr -s " " "\012" | tail -n 1`}".strip
-QJSON_VERSION = "0.7.1"
 
-puts "LIBS version: #{LIBSPOTIFY_VERSION}"
+puts "Using libspotify version: #{LIBSPOTIFY_VERSION}"
 if ARGV.length < 4
   puts "Usage: ruby create_synchrotron.rb spotify_tomahawkresolver metadata.desktop private_key_file /path/to/tomahawk-synchrotron"
   puts "\n"
@@ -32,13 +31,8 @@ platform = "osx"
 puts "Creating zipfile for #{resolver}..."
 
 `cp #{ARGV[0]} #{resolver}`
-# Manually install_name_tool the spotify binary to point to relative paths in the Tomahawk.app bundle
-# This assumes the resolver will be in the Tomahawk.app/Contents/MacOS folder
-`install_name_tool -change /usr/local/Cellar/qt/#{QT_VERSION}/lib/QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore #{resolver}`
-`install_name_tool -change /usr/local/Cellar/qt/#{QT_VERSION}/lib/QtNetwork.framework/Versions/4/QtNetwork @executable_path/../Frameworks/QtNetwork.framework/Versions/4/QtNetwork #{resolver}`
-`install_name_tool -change /usr/local/lib/QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore #{resolver}`
-`install_name_tool -change /usr/local/lib/QtNetwork.framework/Versions/4/QtNetwork @executable_path/../Frameworks/QtNetwork.framework/Versions/4/QtNetwork #{resolver}`
-`install_name_tool -change /usr/local/lib/libqjson.#{QJSON_VERSION}.dylib @executable_path/../Frameworks/libqjson.#{QJSON_VERSION}.dylib #{resolver}`
+# Manually install_name_tool the spotify binary to point to relative paths.
+# This will make the resolver look for libspotify in the current directory.
 `install_name_tool -change /usr/local/lib/libspotify.#{LIBSPOTIFY_VERSION}.dylib @executable_path/libspotify.#{LIBSPOTIFY_VERSION}.dylib #{resolver}`
 
 LIBSPOTIFY_LOCAL = "libspotify.#{LIBSPOTIFY_VERSION}.dylib"
@@ -61,7 +55,6 @@ manifest["_ZIPFILE_"] = tarball
 manifest["_PLATFORM_"] = platform
 manifest["_TYPE_"] = platform
 manifest["_BINARY_"] = resolver
-
 
 
 resolverDir = "#{ARGV[3]}/resolvers/#{resolvername}-#{platform}/content"
