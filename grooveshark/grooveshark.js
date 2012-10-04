6381bcf6ad2fdff36d0366583796151ddaff5c4b
head7 <inscription@srand.fr>
2012-10-04 17:12:22 +0200

    (remove unused variable changes)

--- grooveshark/grooveshark.js
+++ grooveshark/grooveshark.js
@@ -85,7 +85,6 @@ var defaultDiacriticsRemovalMap = [
     {'base':'y','letters':/[\u0079\u24E8\uFF59\u1EF3\u00FD\u0177\u1EF9\u0233\u1E8F\u00FF\u1EF7\u1E99\u1EF5\u01B4\u024F\u1EFF]/g},
     {'base':'z','letters':/[\u007A\u24E9\uFF5A\u017A\u1E91\u017C\u017E\u1E93\u1E95\u01B6\u0225\u0240\u2C6C\uA763]/g}
 ];
-var changes;
 function removeDiacritics (str) {
     for(var i=0; i<defaultDiacriticsRemovalMap.length; i++) {
         str = str.replace(defaultDiacriticsRemovalMap[i].letters, defaultDiacriticsRemovalMap[i].base);
