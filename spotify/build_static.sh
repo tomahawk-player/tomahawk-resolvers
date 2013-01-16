d0e2e11d3bc3eceddbc410346a41660fe356859d
Leo Franchi <lfranchi@kde.org>
2013-01-16 08:58:30 -0500

    Hardcode qt static path in build scripts

--- spotify/build_static.sh
+++ spotify/build_static.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir static
 pushd static
-cmake -DQJSON_INCLUDE_DIR=/Users/leo/src/qjson/static/include/ -DQJSON_LIBRARIES=/Users/leo/src/qjson/static/lib/libqjson.a ..
+PATH=/Users/leo/src/qt-static/bin:$PATH cmake -DQJSON_INCLUDE_DIR=/Users/leo/src/qjson/static/include/ -DQJSON_LIBRARIES=/Users/leo/src/qjson/static/lib/libqjson.a ..
 make -j10
 popd
