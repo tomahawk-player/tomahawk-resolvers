fa3531e785283f6afc8b2e82254a0882bd61986e
AltarBeastiful <r3m1.benoit@gmail.com>
2013-03-06 16:26:17 +0100

    Use downloadLink to play google drive files

--- googledrive/googledrive.js
+++ googledrive/googledrive.js
@@ -96,7 +96,7 @@ var GoogleDriveResolver = Tomahawk.extend(TomahawkResolver, {
         
         musicManager.showDatabase();
               
-        this.googleDriveMusicManagerTests() ; 
+        //this.googleDriveMusicManagerTests() ; 
         
         Tomahawk.addCustomUrlHandler( "googledrive", "getStreamUrl" );
         Tomahawk.reportCapabilities( TomahawkResolverCapability.Browsable | TomahawkResolverCapability.AccountFactory );
@@ -130,6 +130,7 @@ var GoogleDriveResolver = Tomahawk.extend(TomahawkResolver, {
 			if(item['deleted']){
 					Tomahawk.log("Deleting : " + item['fileId']);
 					//dbSQL.deleteTrack(item.file.id);
+					musicManager.deleteTrack({'id' : item['fileId']});
 			}else{		
 				var file = item['file'];
 				//Tomahawk.log("File : " + item['file']['title']+ " is supported : " + this.isMimeTypeSupported(item['file']['mimeType']));
@@ -222,8 +223,9 @@ var GoogleDriveResolver = Tomahawk.extend(TomahawkResolver, {
     
     getStreamUrl: function (ourUrl) {
         var songId = ourUrl.replace("googledrive://id/", "");
+        var meta = JSON.parse(this.oauth.ogetSyncJSON('https://www.googleapis.com/drive/v2/files/' + songId));
         
-		return(this.oauth.createOauthUrl('https://docs.google.com/uc?export=download&id=' + songId)) ;
+		return(this.oauth.createOauthUrl(meta['downloadUrl'])) ;
         
     },
 	
@@ -323,7 +325,6 @@ var GoogleDriveResolver = Tomahawk.extend(TomahawkResolver, {
     	},
     	
     	ogetJSON: function(url, success){
-			//var that = this;
     		if(!this.isAssociated()){
     			//TODO throw error NoAccountAssociated ?
     			Tomahawk.log("REFUSED Get to "+ url + " : No account associated");
@@ -336,6 +337,21 @@ var GoogleDriveResolver = Tomahawk.extend(TomahawkResolver, {
 					Tomahawk.asyncRequest(url, function (data) {
 													success(JSON.parse(data.responseText));
 											   }, {'Authorization': 'Bearer '+ this.accessToken});
+				}
+			}
+    	},
+    	
+		ogetSyncJSON: function(url){
+    		if(!this.isAssociated()){
+    			//TODO throw error NoAccountAssociated ?
+    			Tomahawk.log("REFUSED Get to "+ url + " : No account associated");
+			}else{
+				if(this.tokenExpired()){
+					Tomahawk.log("Token expired");
+    				this.getRefreshedAccessToken();
+				}else{
+					//TODO treat case no parameters given
+					return Tomahawk.syncRequest(url, {'Authorization': 'Bearer '+ this.accessToken});
 				}
 			}
     	},
