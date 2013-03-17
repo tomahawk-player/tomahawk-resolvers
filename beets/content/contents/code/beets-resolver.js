088d4cc051a5ad0aae172caa1b76879011b1c720
Uwe L. Korn <uwelk@xhochy.com>
2013-03-17 19:10:01 +0100

    Add copyright information

--- beets/content/contents/code/beets-resolver.js
+++ beets/content/contents/code/beets-resolver.js
@@ -1,3 +1,22 @@
+/* === This file is part of Tomahawk Player - <http://tomahawk-player.org> ===
+ *
+ *   Copyright 2012, Adrian Sampson <adrian@radbox.org>
+ *   Copyright 2013, Uwe L. Korn <uwelk@xhochy.com>
+ *
+ *   Tomahawk is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Tomahawk is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Tomahawk. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // Map all the audio types supported by beets to extensions and MIME types.
 var AUDIO_TYPES = {
     'MP3':      ['mp3',  'audio/mpeg'],
