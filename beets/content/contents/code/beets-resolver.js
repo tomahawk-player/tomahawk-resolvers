f956d2e82568970e9fa13d2e2865bbddbc2e3ac3
Uwe L. Korn <uwelk@xhochy.com>
2013-03-17 19:06:04 +0100

    [beets] jslint coding style

--- beets/content/contents/code/beets-resolver.js
+++ beets/content/contents/code/beets-resolver.js
@@ -9,11 +9,8 @@ var AUDIO_TYPES = {
     'MusePack': ['mpc',  'audio/x-musepack']
 };
 
-var BeetsResolver = Tomahawk.extend(TomahawkResolver,
-{
-    // Basic setup.
-    settings:
-    {
+var BeetsResolver = Tomahawk.extend(TomahawkResolver, {
+    settings: {
         name: 'beets',
         icon: 'beets-icon.png',
         weight: 95,
@@ -21,58 +18,53 @@ var BeetsResolver = Tomahawk.extend(TomahawkResolver,
     },
 
     // Resolution.
-    resolve: function( qid, artist, album, title )
-    {
-        this.beetsQuery(qid,
-            ['artist:' + artist, 'album:' + album, 'title:' + title]
-        );
+    resolve: function (qid, artist, album, title) {
+        this.beetsQuery(qid, ['artist:' + artist, 'album:' + album, 'title:' + title]);
     },
-    search: function( qid, searchString )
-    {
+
+    search: function (qid, searchString) {
         this.beetsQuery(qid, searchString.split(' '));
     },
-    baseUrl: function() {
+
+    baseUrl: function () {
         return 'http://' + this.host + ':' + this.port;
     },
-    beetsQuery: function( qid, queryParts )
-    {
-        var baseUrl = this.baseUrl();
-        var url = baseUrl + '/item/query/';
-        for (var i = 0; i < queryParts.length; ++i) {
-            url += encodeURIComponent(queryParts[i]);
+
+    beetsQuery: function (qid, queryParts) {
+        var baseUrl = this.baseUrl(),
+            url = this.baseUrl() + '/item/query/';
+        queryParts.forEach(function (item) {
+            url += encodeURIComponent(item);
             url += '/';
-        }
+        });
         url = url.substring(0, url.length - 1);  // Remove last /.
 
-        Tomahawk.asyncRequest(url, function(xhr) {
-            var resp = JSON.parse(xhr.responseText);
-            var items = resp['results'];
-
-            var searchResults = [];
-            for (var i = 0; i < items.length; ++i) {
-                item = items[i];
-                var type_info = AUDIO_TYPES[item['format']];
+        Tomahawk.asyncRequest(url, function (xhr) {
+            var resp = JSON.parse(xhr.responseText),
+                items = resp.results,
+                searchResults = [];
+            items.forEach(function (item) {
+                var type_info = AUDIO_TYPES[item.format];
                 searchResults.push({
-                    artist: item['artist'],
-                    album: item['album'],
-                    track: item['title'],
-                    albumpos: item['track'],
+                    artist: item.artist,
+                    album: item.album,
+                    track: item.title,
+                    albumpos: item.track,
                     source: "beets",
-                    url: baseUrl + '/item/' + item['id'] + '/file',
-                    bitrate: Math.floor(item['bitrate'] / 1024),
-                    duration: Math.floor(item['length']),
-                    size: (item['size'] || 0),
+                    url: baseUrl + '/item/' + item.id + '/file',
+                    bitrate: Math.floor(item.bitrate / 1024),
+                    duration: Math.floor(item.length),
+                    size: (item.size || 0),
                     score: 1.0,
                     extension: type_info[0],
                     mimetype: type_info[1],
-                    year: item['year']
+                    year: item.year
                 });
-            }
-
+            });
             Tomahawk.addTrackResults({
                 qid: qid,
                 results: searchResults
-            })
+            });
         });
     },
 
@@ -98,7 +90,7 @@ var BeetsResolver = Tomahawk.extend(TomahawkResolver,
     init: function () {
         var userConfig = this.getUserConfig();
         this.host = userConfig.host || 'localhost';
-        this.port = parseInt(userConfig.port);
+        this.port = parseInt(userConfig.port, 10);
         if (isNaN(this.port) || !this.port) {
             this.port = 8337;
         }
