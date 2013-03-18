66db813064bf25c8b76a524ae7868456d0dec355
Teo Mrnjavac <teo@kde.org>
2013-03-18 10:02:10 +0100

    License headers.

--- dilandau/content/contents/code/dilandau.js
+++ dilandau/content/contents/code/dilandau.js
@@ -1,8 +1,23 @@
-/*
- * (c) 2011 lasconic <lasconic@gmail.com>
- * (c) 2011 leo franchi <lfranchi@kde.org>
- * (c) 2012 thierry göckel <thierry@strayrayday.lu>
+/* === This file is part of Tomahawk Player - <http://tomahawk-player.org> ===
+ *
+ *   Copyright 2011, lasconic <lasconic@gmail.com>
+ *   Copyright 2011, Leo Franchi <lfranchi@kde.org>
+ *   Copyright 2012, Thierry Göckel <thierry@strayrayday.lu>
+ *
+ *   Tomahawk is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Tomahawk is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Tomahawk. If not, see <http://www.gnu.org/licenses/>.
  */
+
 var DilandauResolver = Tomahawk.extend(TomahawkResolver, {
 
 	settings: {
