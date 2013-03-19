be0dd4fda288e477a1a2d3db25ac273113b181f5
Teo Mrnjavac <teo@kde.org>
2013-03-19 16:56:10 +0100

    Bundlify 8tracks.

--- 8tracks/content/contents/code/8tracks.js
+++ 8tracks/content/contents/code/8tracks.js
@@ -1,7 +1,22 @@
-/*
- * (c) 2011 Janez Troha (https://github.com/dz0ny)
- * (c) 2011 Leo Franchi <lfranchi@kde.org)
+/* === This file is part of Tomahawk Player - <http://tomahawk-player.org> ===
+ *
+ *   Copyright 2011, Janez Troha (https://github.com/dz0ny)
+ *   Copyright 2011, Leo Franchi <lfranchi@kde.org>
+ *
+ *   Tomahawk is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Tomahawk is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Tomahawk. If not, see <http://www.gnu.org/licenses/>.
  */
+
 var EightTracksResolver = Tomahawk.extend(TomahawkResolver, {
     settings: {
         name: '8tracks Resolver',
