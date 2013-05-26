c1120d42f866086ac97cb8ee0c2e8a5acc7d544c
Uwe L. Korn <uwelk@xhochy.com>
2013-05-26 19:07:57 +0200

    [exfm] Return all results on resolving without checking
    * Remove async.js
    * Use Tomahawk's internal result checker

--- exfm/content/contents/code/exfm-resolver.js
+++ exfm/content/contents/code/exfm-resolver.js
@@ -107,31 +107,7 @@ var ExfmResolver = Tomahawk.extend(TomahawkResolver, {
                 });
             }
 
-            if (artist !== "") {
-                // We are in resolve mode, so return only the first correct result.
-                async.reduce(results, null, function (memo, item, callback) {
-                    if (memo === null) {
-                        that.checkUrl(item, function (success) {
-                            if (success) {
-                                callback(null, item);
-                            } else {
-                                callback(null, memo);
-                            }
-                        });
-                    } else {
-                        callback(null, memo);
-                    }
-                }, function (err, result) {
-                    if (result !== null) {
-                        // In resolve mode the first best result is returned.
-                        Tomahawk.addTrackResults({qid: qid, results: [result]});
-                    }
-                });
-            } else {
-                // TODO: Check all results
-                Tomahawk.addTrackResults({qid: qid, results: results});
-            }
-
+            Tomahawk.addTrackResults({qid: qid, results: results});
         });
     },
 
