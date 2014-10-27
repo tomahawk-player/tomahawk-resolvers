5b139e488cdb13bb6dd5e0211b94e1ed77412e30
Uwe L. Korn <uwelk@xhochy.com>
2014-10-27 15:58:55 +0100

    [tools] Add URLLookup tool

---
+++ tools/urllookp.js
@@ -0,0 +1,40 @@
+var argv = require('minimist')(process.argv.slice(2));
+var utils = require('../test/utils.js');
+
+if (argv._.length < 1) {
+    console.error("Please specify a single resolver");
+    process.exit(1);
+}
+
+if (argv._.length < 2) {
+    console.error("You need to specify an URL which shall be looked up.");
+    process.exit(1);
+}
+
+// We use the string argument as the path to the resolver.
+var resolverPath = argv._[0];
+var url = argv._[1];
+
+// All -/-- arguments will be added to the config.
+// TODO: Add an agurment to load the config from a file.
+var resolverConfig = argv;
+delete resolverConfig._;
+
+var resolver = {};
+
+utils.loadAxe(resolverPath, resolver, function () {
+    if (!resolver.context.hasCapability('urllookup')) {
+        console.error("Resolver does not support URLLookup.");
+    } else {
+        resolver.context.on('url-result', function (url, result) {
+            console.dir(result);
+        });
+
+        if (resolver.instance.canParseUrl(url)) {
+            resolver.instance.lookupUrl(url);
+        } else {
+            console.error("Resolver cannot lookup URLs of the given type.");
+        }
+    }
+}, resolverConfig);
+
