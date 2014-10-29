89e01573c8945498d5adb3d762eecbdb03af886b
Uwe L. Korn <uwelk@xhochy.com>
2014-10-29 19:18:04 +0100

    [authproxy] Add basic Spotify handling

--- authproxy/authproxy.go
+++ authproxy/authproxy.go
@@ -6,6 +6,7 @@ import (
 
 func init() {
     http.HandleFunc("/json", jsonHandler)
+    http.HandleFunc("/spotify", spotifyHandler)
 }
 
 
@@ -13,4 +14,10 @@ func jsonHandler(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
     w.Header().Set("Access-Control-Allow-Origin", "*")
     w.Write([]byte("{ \"access_token\": \"" + r.FormValue("access_token") + "\" }"))
+}
+
+func spotifyHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Write([]byte("{ \"code\": \"" + r.FormValue("code") + "\" }"))
 }
