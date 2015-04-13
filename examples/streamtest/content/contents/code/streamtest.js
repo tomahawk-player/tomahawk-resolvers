b8412caa3a1a5abb86a1e0e4468075396f09d9be
Anton Romanov <theli.ua@gmail.com>
2015-04-13 13:32:56 -0700

    Turn HLS streaming plugin into general purpose test streaming one

---
+++ examples/streamtest/content/contents/code/streamtest.js
@@ -0,0 +1,50 @@
+var StreamTestResolver = Tomahawk.extend( Tomahawk.Resolver.Promise, {
+    apiVersion: 0.9,
+
+    settings: {
+        cacheTime: 300,
+        name: 'Stream Test',
+        icon: '../icon.png',
+        weight: 10,
+        timeout: 8
+    },
+
+    getConfigUi: function() {
+        return {
+            "widget": Tomahawk.readBase64( "config.ui" ),
+            fields: [{
+                name: "stream_url",
+                widget: "url_edit",
+                property: "text"
+            }]
+        };
+    },
+
+    init: function() {
+        Tomahawk.reportCapabilities(TomahawkResolverCapability.UrlLookup);
+        Tomahawk.addCustomUrlHandler( 'streamtest', 'getStreamUrl', true );
+    },
+
+    resolve: function (artist, album, title) {
+        return [{
+            artist:     artist,
+            album:      album,
+            track:      title,
+            title:      title,
+
+            url:        'streamtest://track/' + encodeURIComponent(title),
+            hint:       'streamtest://track/' + encodeURIComponent(title),
+            checked:    true,
+            bitrate:    64,
+            type:       "track",
+        }];
+    },
+
+    getStreamUrl: function(qid, url) {
+        var config = this.getUserConfig();
+        Tomahawk.reportStreamUrl(qid, config.stream_url);
+    },
+
+});
+
+Tomahawk.resolver.instance = StreamTestResolver;
