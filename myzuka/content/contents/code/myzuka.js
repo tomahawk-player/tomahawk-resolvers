622c2a849af446de837571142e403c22d33d1012
Anton Romanov <theli.ua@gmail.com>
2015-04-24 18:47:25 -0700

    Use different search frontend to get duration, bitrate and filesize

--- myzuka/content/contents/code/myzuka.js
+++ myzuka/content/contents/code/myzuka.js
@@ -26,23 +26,31 @@ var MyzukaResolver = Tomahawk.extend( Tomahawk.Resolver.Promise, {
         var that = this;
 
         var params = {
-            searchText: query
+            q: query,
+            p: '1',
+            aux: 'PLA099'
         };
 
-        return Tomahawk.get(this.apiLocation + "Search", {
+        return Tomahawk.get("https://go.mail.ru/search_site", {
             data: params
         }).then( function (response) {
             var results = [];
-            var trackRe = /<a\ +href="\/Artist\/[^"]+">([^<]+)<[\s\S]*?<a\ +href="\/?(Song[^"]+)">([^<]+)/gm;
-            var tracks = response.substring(response.indexOf("Поиск по композициям"), response.length);
+            var trackRe = /href="https?:\/\/myzuka.org\/(Song[^"]+)">([^<]+) - ([^<]+)[\s\S]*?:[^0-9]+([0-9]+,[0-9]+)[^:]+:\ +([0-9:]+)[^:]+:\ +([0-9]+)/gm;
             var matches;
+            var tracks = response.substring(response.indexOf("result__banner"), response.length);
             while (matches = trackRe.exec(tracks)) {
+                var time = matches[5].split(':');
+                var min = parseInt(time[0]);
+                var sec = parseInt(time[1]);
                 results.push({
-                    type: 'track',
-                    artist: matches[1],
-                    title: matches[3],
-                    track: matches[3],
-                    url: 'myzuka://' + matches[2],
+                    type:       'track',
+                    artist:     matches[2],
+                    title:      matches[3],
+                    track:      matches[3],
+                    url:        'myzuka://' + matches[1],
+                    duration:   min * 60 + sec,
+                    bitrate:    parseInt(matches[6]),
+                    size:       parseFloat(matches[4].replace(",", ".")) * 1024 * 1024,
                 });
             }
 
@@ -71,7 +79,6 @@ var MyzukaResolver = Tomahawk.extend( Tomahawk.Resolver.Promise, {
         Tomahawk.get(songPageUrl).then(function(response) {
             var mediaRe = /<a itemprop="audio" href="([^"]+)/gm;
             var match = mediaRe.exec(response);
-            Tomahawk.log(JSON.stringify(match));
             var streamUrl = match[1].replace(/&amp;/g, '&');
             if (streamUrl.substring(0, 4) != "http") {
                 streamUrl = that.apiLocation + streamUrl;
